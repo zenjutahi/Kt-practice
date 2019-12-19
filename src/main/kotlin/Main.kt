2e96ee83eb1e95be026c577ed21e9e739ae6f30d
zenjutahi <jeffmnjogu@gmail.com>
2019-12-19 12:20:51 +0300

    [chore] setting up kotlin project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+fun main(args: Array<String>) {
+    println("Hello World")
+    println("This is my second kotlin program")
+
+    var zenj: String = "Zenjutahi Njogu"
+    var zenjsMath: Int = 75 * 10
+    println(zenj)
+    println(zenjsMath)
+
+    println()
+
+    val apples: Int = 10
+    val oranges: Int = 3
+    var sumOfFruit: Int = apples + oranges
+    println("Average of the fruits is: ${sumOfFruit / 3}")
+
+    // produces decimals when we use Double lable
+    val days: Int = 141
+    val weeks: Double = days / 7.0
+    println("$days days in weeks is $weeks weeks" )
+
+    println()
+    println("My name is $zenj")
+
+
+
+}
